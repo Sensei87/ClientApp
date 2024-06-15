357312811c796d03066b4150d83016b45e2517f7
Sergii Povarchuk <sergii.povarchuk@computools.com>
2024-06-15 11:57:26 +0300

    Add project files.

---
+++ ClientApp/Client.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Concurrent;
+
+namespace ClientApp
+{
+    public static class Client
+    {
+        // This client method receives data from the server
+        public static void Receiver(string? multicastAddress, int port)
+        {
+            ConcurrentBag<double> data = new ConcurrentBag<double>();
+            CancellationTokenSource cts = new CancellationTokenSource();
+            using (UdpClient udpClient = new UdpClient(port))
+            {
+                udpClient.JoinMulticastGroup(IPAddress.Parse(multicastAddress));
+
+                Task.Run(() =>
+                {
+                    while (!cts.Token.IsCancellationRequested)
+                    {
+                        IPEndPoint remoteEndPoint = new IPEndPoint(IPAddress.Any, 0);
+                        byte[] receivedBytes = udpClient.Receive(ref remoteEndPoint);
+                        string receivedData = Encoding.ASCII.GetString(receivedBytes);
+                        if (double.TryParse(receivedData, out double result))
+                        {
+                            data.Add(result);
+                        }
+                    }
+                }, cts.Token);
+
+                while (!cts.Token.IsCancellationRequested)
+                {
+                    if (Console.ReadKey().Key == ConsoleKey.Enter)
+                    {
+                        var fixedCollection = data.ToArray();
+                        double average = fixedCollection.Average();
+                        double median = fixedCollection.OrderBy(x => x).Skip(fixedCollection.Length / 2).First();
+                        double mode = fixedCollection.GroupBy(x => x).OrderByDescending(g => g.Count()).First().Key;
+                        double standardDeviation = Math.Sqrt(fixedCollection.Average(x => Math.Pow(x - average, 2)));
+
+                        Console.WriteLine($"Average: {average}");
+                        Console.WriteLine($"Median: {median}");
+                        Console.WriteLine($"Mode: {mode}");
+                        Console.WriteLine($"Standard deviation: {standardDeviation}");
+                    }
+                    else if (Console.ReadKey().Key == ConsoleKey.Escape)
+                    {
+                        cts.Cancel();
+                    }
+                }
+            }
+        }
+
+
+
+    }
+}
---
+++ ClientApp/Program.cs
@@ -0,0 +1,59 @@
+﻿
+using ClientApp;
+using System.Xml;
+using System.Xml.Linq;
+
+Console.WriteLine("Receive data from Server");
+
+// This is a technical task for an interview.
+// Range, port and multicast group  config
+
+try
+{
+    string? path = Environment.CurrentDirectory;
+
+    // Find the file in the local directories
+    DirectoryInfo directory = new DirectoryInfo(path);
+    DirectoryInfo? parentDirectory = directory.Parent;
+    DirectoryInfo? projectDirectory = parentDirectory?.Parent;
+    DirectoryInfo? xmlLocation = projectDirectory?.Parent;
+
+
+    if (xmlLocation == null)
+    {
+        Console.WriteLine("Can't to find the XML location.");
+        return;
+    }
+
+    XDocument document = XDocument.Load(Path.Combine(xmlLocation.FullName, "config.xml"));
+
+    XElement? root = document.Root;
+    if (root == null)
+    {
+        Console.WriteLine("Element not found in XML.");
+        return;
+    }
+
+    string? multicastAddress = root.Element("multicastAddress")?.Value;
+
+    if (!int.TryParse(root.Element("port")?.Value, out int port))
+    {
+        Console.WriteLine("Invalid port number.");
+        return;
+    }
+
+
+    Client.Receiver(multicastAddress, port);
+
+
+}
+catch (IOException ex)
+{
+    Console.WriteLine($"IO Exception: {ex.Message}");
+}
+catch (XmlException ex)
+{
+    Console.WriteLine($"XML Exception: {ex.Message}");
+}
+
+
